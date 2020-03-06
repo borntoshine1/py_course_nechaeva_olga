c2a28afffa36f8f86e9939b8b367b8f01b9ee0af
borntoshine1 <61425845+borntoshine1@users.noreply.github.com>
2020-03-06 11:33:25 +0200

    Update equation_solution.py

--- homework/hw4/equation_solution.py
+++ homework/hw4/equation_solution.py
@@ -1,8 +1,9 @@
 a = int(input('Enter a: '))
 b = int(input('Enter b: '))
-if a>0 and b>=0:
-	x = -b/a
-	print('x value is - ' + str(x))
-elif a == 0:
-	print('No solutions')
-	
+if a>0 or a<0:
+    x = -b/a
+    print('x value is - {}'.format(x))
+elif a == 0 and b == 0:
+    print('X any value')
+else:
+    print('No solutions')
