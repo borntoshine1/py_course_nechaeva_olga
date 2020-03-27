b638afa6aafcae2811a194ef1cecc9a8ed46db29
borntoshine1 <61425845+borntoshine1@users.noreply.github.com>
2020-03-27 19:10:03 +0200

    Update task_1.py

--- homework/hw6/task_1.py
+++ homework/hw6/task_1.py
@@ -1,13 +1,4 @@
 lst = ['i love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all']
 
 num = int(input("Enter number - "))
-
-i = 1
-j = 0
-while i <= num:
-    if j == len(lst):
-        j = 0
-    i+=1
-    j+=1
-print(lst[j-1])
-    
+print(lst[(num%len(lst))-1])
