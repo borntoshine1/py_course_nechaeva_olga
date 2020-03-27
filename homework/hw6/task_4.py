0a083d8a688c9d8b86edbcda01c0d5d415078969
borntoshine1 <61425845+borntoshine1@users.noreply.github.com>
2020-03-27 19:22:07 +0200

    Update task_4.py

--- homework/hw6/task_4.py
+++ homework/hw6/task_4.py
@@ -1,21 +1,8 @@
-word = "olyylo"
+word = "oleyylo"
 #word = input("Enter word - ")
 
 #list(word)
-lst = list(word)
-i=0
-j = len(lst)
-count = False
-while i <= j:
-    if lst[i] == lst[j-1]:
-        i+=1
-        j-=1
-        count = True
-    else:
-        count = False
-        break
-
-if count == True:
-    print("Еhis word is a palindrome")
+if word == word[::-1]:
+    print("This word is a palindrome")
 else:
-    print("Not palindrome")
+    print("Not palindrome")     
